8f66cc402f57e265ec633c134272e2d33bcbbeeb
Ankush Mehta <mail@nksh.io>
2019-01-23 00:44:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Put your login into a file called `login.json`.
+
+I should look like following:
+```
+{
+    "user": "user",
+    "pass": "pass"
+}
+```
+
+
+then run `npm install && node login.js`.
+
+
+Run this as a cron job for 100 days to get your **Fanatic** badge :)
---
+++ login.js
@@ -0,0 +1,22 @@
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({
+  show: true
+});
+const fs = require('fs');
+
+const {
+  user,
+  pass
+} = JSON.parse(fs.readFileSync(`${__dirname}/login.json`, 'utf8'));
+
+nightmare
+  .goto('https://stackoverflow.com/users/login')
+  .type('#email', user)
+  .type('#password', pass)
+  .click('#submit-button')
+  .wait(5000)
+  .end()
+  .then(console.log("Login Successful, see you tomorrow"))
+  .catch((error) => {
+    console.error('Login Failed:', error);
+  });
